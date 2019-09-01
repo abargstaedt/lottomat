62e05a15b2b19157cf668690f3f7bc1e12e2d727
abargstaedt <andreas@bargstaedt.net>
2019-09-01 19:28:39 +0200

    Refactoring

--- main.go
+++ main.go
@@ -6,22 +6,30 @@ import (
 	"sort"
 )
 
-func newField(remainingBalls *[]int) []int {
-	field := make([]int, 6)
+func drawNumbers(remainingNumbers *[]int) []int {
+	numbers := make([]int, 6)
 	for i := 0; i < 6; i++ {
-		n := rand.Intn(len(*remainingBalls))
-		field[i] = (*remainingBalls)[n]
-		*remainingBalls = append((*remainingBalls)[:n], (*remainingBalls)[n+1:]...)
+		n := rand.Intn(len(*remainingNumbers))
+		numbers[i] = (*remainingNumbers)[n]
+		*remainingNumbers = append((*remainingNumbers)[:n], (*remainingNumbers)[n+1:]...)
 	}
-	sort.Ints(field)
-	return field
+	sort.Ints(numbers)
+	return numbers
+}
+
+func getFields(remainingNumbers *[]int) [][]int {
+	fields := make([][]int, 8)
+	for i := 0; i < 8; i++ {
+		fields[i] = drawNumbers(remainingNumbers)
+	}
+	return fields
 }
 
 func main() {
-	allBalls := [49]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49}
-	remainingBalls := allBalls[:]
-	for f := 0; f < 8; f++ {
-		fmt.Println("Feld", f+1, ":", newField(&remainingBalls))
+	allNumbers := [49]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49}
+	remainingNumbers := allNumbers[:]
+	for index, field := range getFields(&remainingNumbers) {
+		fmt.Println("Feld", index+1, ":", field)
 	}
-	fmt.Println("Übrig :", remainingBalls)
+	fmt.Println("Übrig :", remainingNumbers)
 }
