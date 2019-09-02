247dff56b4cb30917ecae15b313daa2d4da26b01
abargstaedt <andreas@bargstaedt.net>
2019-09-02 05:39:44 +0200

    Update main.go
    Refactoring

--- main.go
+++ main.go
@@ -34,15 +34,17 @@ func (t ticket) String() (s string) {
 }
 
 func main() {
-	fmt.Print(getTicket())
+	fmt.Println(getTicket())
 }
 
 func getTicket() (t ticket) {
 	allNumbers := [49]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49}
-	t.fields = make([]field, 8)
 	t.remainingNumbers = allNumbers[:]
-	for i := 0; i < 8; i++ {
-		t.fields[i] = drawNumbers(&t.remainingNumbers)
+	for {
+		t.fields = append(t.fields, drawNumbers(&t.remainingNumbers))
+		if len(t.remainingNumbers) < 6 {
+			break
+		}
 	}
 	return
 }
